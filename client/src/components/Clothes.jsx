20152a9527d398c1082acf038c5cf912767bb623
byungmin12 <kbm940526@naver.com>
2021-10-22 23:52:01 +0900

    add code about redux, add function to open modal about washing clothes and  modify css

--- client/src/components/Clothes.jsx
+++ client/src/components/Clothes.jsx
@@ -1,20 +1,38 @@
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
 import { useDrag } from 'react-dnd';
+import { useDispatch } from 'react-redux';
+import { handleOpenClothesModal } from '../actions/ClotheModalActionIndex';
 
 const Img = styled.div`
-  background-color: transparent;
-  width: 200px;
-  height: 200px;
+  display: inline-block;
+  width: 155px;
+  height: 155px;
+  margin: 30px;
   ${({ data }) => {
     return data ? `background-image: url(${data.url}); background-size: 100% 100%; ` : null;
   }};
   ${({ isDragging }) => {
     return isDragging ? `opacity: 0.4;` : `opacity: 1;`;
   }};
+  background-color: transparent;
+  position: relative;
+  z-index: 1;
+  :hover {
+    transform: scale(1.1);
+    cursor: pointer;
+  }
+  align-self: center;
+  justify-self: center;
+
+  /* -webkit-filter: drop-shadow(#ffffff -3px -4px 10px);
+  filter: drop-shadow(#757ea0 3px 4px 10px); */
+  box-shadow: #ffffff -3px -4px 10px 0px, #757ea0 3px 4px 10px 0px;
+  border-radius: 30px;
 `;
 
 function Clothes({ data }) {
+  const dispatch = useDispatch();
   const [{ isDragging }, drag] = useDrag(() => ({
     type: 'image',
     item: { id: data.id },
@@ -27,7 +45,11 @@ function Clothes({ data }) {
     },
   }));
 
-  return <Img ref={drag} isDragging={isDragging} data={data}></Img>;
+  const handleModal = () => {
+    dispatch(handleOpenClothesModal(data.id));
+  };
+
+  return <Img onClick={handleModal} ref={drag} isDragging={isDragging} data={data}></Img>;
 }
 
 export default Clothes;
--- client/src/components/ClothesContainer.jsx
+++ client/src/components/ClothesContainer.jsx
@@ -1,61 +1,108 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
 import styled, { keyframes } from 'styled-components';
 import Clothes from './Clothes';
 
-const ClothesContainerCss = styled.div`
-  width: 46%;
+const Container = styled.div`
+  width: 50%;
   height: 90%;
-  background-color: transparent;
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-auto-rows: auto;
-  gap: 10px 20px;
-  overflow-x: scroll;
-  grid-auto-flow: row;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  padding: 20px;
 `;
 
-const ClothesData = [
-  {
-    id: 1,
-    url: '../../clothes.png',
-  },
-  {
-    id: 2,
-    url: '../../clothes.png',
-  },
-  {
-    id: 3,
-    url: '../../clothes.png',
-  },
-  {
-    id: 4,
-    url: '../../clothes.png',
-  },
-  {
-    id: 5,
-    url: '../../clothes.png',
-  },
-  {
-    id: 6,
-    url: '../../clothes.png',
-  },
-  {
-    id: 7,
-    url: '../../clothes.png',
-  },
-  {
-    id: 8,
-    url: '../../clothes.png',
-  },
-];
+const ClothesContainerCss = styled.div`
+  width: 100%;
+  height: 95%;
+  /* background-color: red; */
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+  gap: 10px 20px;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const Loading = styled.div`
+  width: 100%;
+  height: 20%;
+  /* margin: 50px auto; */
+  /* text-align: center; */
+  position: absolute;
+  top: 50%;
+  transform: translate(0%, -50%);
+  z-index: 150;
+  background-color: rgba(255, 255, 255, 0.3);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.5rem;
+  font-weight: 800;
+`;
 
 function ClothesContainer() {
+  const state = useSelector((state) => state.WashingMachineReducer);
+
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const handleScroll = (e) => {
+    console.log(e.currentTarget.scrollHeight, '스크롤높이');
+    console.log(e.currentTarget.clientHeight, '클라이언트높이');
+    console.log(e.currentTarget.scrollTop, '스크롤탑');
+
+    const { scrollTop, clientHeight, scrollHeight } = e.currentTarget;
+    if (scrollHeight - scrollTop === clientHeight) {
+      setPage((prev) => prev + 1);
+      console.log('페이지 업');
+    }
+  };
+
+  useEffect(() => {
+    //여기서 계속 추가를 해줘야함
+    const listSliceLength = 9;
+    const getData = () => {
+      setTimeout(() => {
+        let remainder = state.ClothesData.slice(listSliceLength);
+        let list = state.ClothesData.slice((page - 1) * listSliceLength, page * listSliceLength);
+        console.log('데이터 가져오기');
+        console.log('나머지', remainder);
+        console.log('리스트', list);
+
+        //9개를 뽑기전에 전체 길이가 9 보다 작은 경우
+        // 9보다 많은 경우
+        if (remainder.length >= 12) {
+          setData((prev) => [...prev, ...list]);
+        } else {
+          list = state.ClothesData.slice(0, state.ClothesData.length);
+          setData((prev) => [...prev, ...list]);
+        }
+        setIsLoading(false);
+      }, 1000);
+    };
+    const loadData = async () => {
+      setIsLoading(true);
+      getData();
+      // setIsLoading(false);
+    };
+    loadData();
+  }, [page]);
+
   return (
-    <ClothesContainerCss>
-      {ClothesData.map((el) => {
-        return <Clothes data={el} />;
-      })}
-    </ClothesContainerCss>
+    <Container>
+      <ClothesContainerCss onScroll={(e) => handleScroll(e)}>
+        {data.map((el) => {
+          return <Clothes data={el} key={el.id} />;
+        })}
+      </ClothesContainerCss>
+      {isLoading ? <Loading>Loading...</Loading> : null}
+      {/* <Loading>Loading...</Loading> */}
+    </Container>
   );
 }
 
