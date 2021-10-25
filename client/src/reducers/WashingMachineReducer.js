3c3f08f72ab770ad74d71484a7d62af96912356d
byungmin12 <kbm940526@naver.com>
2021-10-25 22:17:32 +0900

    divide code about open and close cap

--- client/src/reducers/WashingMachineReducer.js
+++ client/src/reducers/WashingMachineReducer.js
@@ -1,13 +1,21 @@
 import { ClothesInitialState } from './initialState';
-import { HANDLE_OPEN_CLOSE_WASHINGMACHINE } from '../actions/WashingMachineActionIndex';
+// import { HANDLE_OPEN_CLOSE_WASHINGMACHINE } from '../actions/WashingMachineActionIndex';
+import { HANDLE_SEARCH_CLOTHES } from '../actions/WashingMachineActionIndex';
 
 const WashingMachineReducer = (state = ClothesInitialState, action) => {
   const newState = { ...state };
   switch (action.type) {
-    case HANDLE_OPEN_CLOSE_WASHINGMACHINE:
-      newState.isOpenClose = action.payload;
-      return newState;
+    // case HANDLE_OPEN_CLOSE_WASHINGMACHINE:
+    //   newState.isOpenClose = action.payload;
+    //   return newState;
 
+    case HANDLE_SEARCH_CLOTHES:
+      if (action.payCheck.length === 0) {
+        return ClothesInitialState;
+      } else {
+        newState.ClothesData = [...action.payload];
+        return newState;
+      }
     default:
       return state;
   }
