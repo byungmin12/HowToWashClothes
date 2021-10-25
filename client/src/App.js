fd965d0a8972cc348622d27417eb7e0ee1033e4e
byungmin12 <kbm940526@naver.com>
2021-10-25 22:38:48 +0900

    add font

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 import { DndProvider } from 'react-dnd';
@@ -5,6 +6,8 @@ import { HTML5Backend } from 'react-dnd-html5-backend';
 import WashingMachine from './components/WashingMachine';
 import ClothesContainer from './components/ClothesContainer';
 import WashInfoModal from './components/WashInfoModal';
+import IntroModal from './components/IntroModal';
+import './font/font.css';
 
 const AppCss = styled.div`
   width: 100vw;
@@ -17,36 +20,40 @@ const AppCss = styled.div`
   justify-content: center;
   align-items: center;
   overflow: hidden;
+
+  background-image: radial-gradient(
+    circle farthest-corner at center,
+    rgba(172, 182, 197, 1) 35%,
+    rgba(196, 178, 168, 1) 65%,
+    rgba(161, 186, 188, 1) 100%
+  );
 `;
 
 const Background = styled.div`
   width: 70%;
   height: 80%;
-  /* border-radius: 30px; */
-
   display: flex;
   align-items: center;
   justify-content: space-evenly;
-
   background: #c4b2a8;
   box-shadow: 28px 28px 55px #a7978f, -28px -28px 55px #e1cdc1;
-
-  @media screen and (max-width: 500px) {
-    flex-direction: column;
-    justify-content: center;
-    width: 90%;
+  position: relative;
+  @media screen and (max-width: 780px) {
+    /* flex-direction: column; */
+    /* justify-content: center; */
+    width: 100%;
   }
 `;
 
 function App() {
   const state = useSelector((state) => state);
-  console.log(state);
   return (
     <DndProvider backend={HTML5Backend}>
       <AppCss>
         <Background>
           <WashingMachine />
           <ClothesContainer />
+          <IntroModal />
         </Background>
         {state.ClothesModalReducer.isModalOpenClose ? (
           <WashInfoModal data={state.ClothesModalReducer.ClothesData[0]} />
